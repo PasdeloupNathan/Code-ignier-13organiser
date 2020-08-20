9e12164056c379b4c10091f0199966bd0026d575
fawzibazari <67472505+fawzibazari@users.noreply.github.com>
2020-08-20 13:58:11 +0200

    naruto

--- application/controllers/connection.php
+++ application/controllers/connection.php
@@ -6,16 +6,27 @@ class Connection extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->database();
-		
-		$resultat = $this->db->query('SELECT * FROM users')->result();
 
-		$data["naruto"] = $resultat;
-
-        $this->load->view('connection_page', $data);
+        $this->load->view('connection_page');
         
 
         
 	}
 	
 }
+
+// public function index()
+// {
+//     $this->load->database();
+    
+//     $resultat = $this->db->query('SELECT * FROM users')->result();
+
+//     $data["naruto"] = $resultat;
+
+//     $this->load->view('connection_page', $data);
+    
+
+    
+// }
+
+// }
